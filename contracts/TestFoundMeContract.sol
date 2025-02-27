630dfd29336b30fabdb9dcbe99b113a798597611
David Pasirstein <6080874+dpasirst@users.noreply.github.com>
2025-02-26 18:12:45 -0700

    feat: test contract

---
+++ contracts/TestFoundMeContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.19;
+
+contract FoundMeContract {
+
+    function foundMe() public pure returns (string memory) {
+        return "You found me on the blockchain!";
+    }
+
+}
---
+++ scripts/testfoundmecontractdeploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const Token = await ethers.getContractFactory("FoundMeContract");
+    const token = await Token.deploy();
+
+    await token.waitForDeployment();
+
+    console.log("Token deployed to:", await token.getAddress());
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
